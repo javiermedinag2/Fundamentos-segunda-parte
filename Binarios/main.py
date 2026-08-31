beca63d3740a29c870291aa99c1e34f3b6ad6f6b
Javier Medina <javiermedinag@hotmail.com>
2026-08-31 08:41:45 -0600

    Lunes 31

---
+++ Binarios/main.py
@@ -0,0 +1,14 @@
+#Ejemplos de operaciones bita a bit
+# 6 es en binario 110
+# 3 es en binario 011
+# 6&3 = 2 (and bit a bit)
+print(6&3)
+
+#or 
+print(6|3)
+
+#desplazar a la izquierda
+print(3<<2)
+
+#desplazar a la derecha
+print(12>>2)
